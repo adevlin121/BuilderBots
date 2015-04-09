e6391438cc2fea81fd9fbfad37fcdf731b663e2a
adevlin121 <adevlin121@hotmail.com>
2015-04-09 23:19:28 +0100

    2d array

--- src/ie/dit/Painterbots.java
+++ src/ie/dit/Painterbots.java
@@ -3,15 +3,46 @@ import processing.core.*;
 
 public class Painterbots  extends PApplet
 {
+	int gridSize = 10;
+	int[][] drawGrid = new int[gridSize][gridSize];
+	int bgColour = color(100, 100, 200);
+	int rectFill = color(255, 255, 255, 70);
+	int rectStroke = color(255);
+	int w = 1200;
+	int h = 800;
+	int gridBox = h/gridSize;
+	int gridStartx = (int) (gridBox * 2.5); 
+	
 	public void setup()
 	{
-		size(200,200);
-		background(0);
-	}
+		size(w,h);
+		
+		//Initializing grid 2d array to be all 0's
+		for(int i=0; i<gridSize; i++)
+		{
+			for(int j=0; j<gridSize; j++)
+			{
+				drawGrid[i][j] = 0;
+			}//end inner for()
+		}//end outer for()
+	}//end setup()
 	
 	public void draw()
 	{
-		stroke(255);
-		rect(10, 10, 20, 20);
-	}
-}
+		background(bgColour);
+		fill(rectFill);
+		stroke(rectStroke);
+		int x = gridStartx;
+		int y = 0;
+		for(int i=0; i<gridSize; i++)
+		{
+			for(int j=0; j<gridSize; j++)
+			{
+				rect(x, y, gridBox, gridBox);
+				x += gridBox;
+			}//end inner for()
+			x = gridStartx;
+			y += gridBox;
+		}//end outer for()
+	}//end draw()
+}//end Painterbots class
